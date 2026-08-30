eef15c7aace680f40781ac629ddab0e959152f16
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-08-30 17:56:13 +0200

    initial commit

---
+++ SearchHelper/SearchHelpers.cs
@@ -0,0 +1,9 @@
+﻿namespace SearchAlgorithmPractice.SearchHelper;
+
+internal static class SearchHelpers
+{
+    internal static IEnumerable<T> LinearSearch<T>(IEnumerable<T> target, IEnumerable<T> array)
+    
+    
+    internal static IReadOnlyList<T> BinarySearch<T>(IEnumerable<T> target, IEnumerable<T> array)
+}
